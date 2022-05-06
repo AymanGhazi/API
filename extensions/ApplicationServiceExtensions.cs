0f945f8872b2976a3386b8e52858cb12632a6928
AymanGhazi <aymanghazi2587@gmail.com>
2022-05-06 21:16:18 +0200

    first commit

---
+++ extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,42 @@
+using API.Data;
+using API.Date;
+using API.DTos;
+using API.Helpers;
+using API.interfaces;
+using API.services;
+using API.SignalR;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection ApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            //SignalR tracker of users and connectionIDs
+            services.AddSingleton<presenceTracker>();
+            //to update last Active filter
+            services.AddScoped<LogUserActivity>();
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            
+            services.AddScoped<IPhotoService, PhotoService>();
+
+            services.AddScoped<ITokenService, TokenService>();
+
+            services.AddAutoMapper(typeof(AutomapperProfiles).Assembly);
+
+            //JWT json web Token
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+    }
+}
